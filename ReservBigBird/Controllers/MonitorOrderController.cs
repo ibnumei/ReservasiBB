6b36d7cb3f3e6e3d8fcb68f7d17bb6272bb662f8
ibnumei <ibnu.fauziyai@gmail.com>
2019-05-09 09:06:43 +0700

    9-5-2019

---
+++ ReservBigBird/Controllers/MonitorOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservBigBird.Controllers
+{
+    public class MonitorOrderController : Controller
+    {
+        // GET: MonitorOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
